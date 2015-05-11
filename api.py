4bab89f6bd3b77fd12b0947a7ea1cf48f7d912f7
ryanfmurphy <ryanfmurphy@gmail.com>
2015-05-10 20:35:24 -0500

    woot API much better! /urls/search: search by string (regex?) /urls/tag: search by tag /urls: show ALL urls /urls/tags: show tags and # occurrences

--- api.py
+++ api.py
@@ -11,6 +11,23 @@ url_re = 'https?://\S+'
 @app.route('/urls')
 def urls():
     with open('urls.txt', 'r') as fh:
+        lines = fh.readlines()
+
+    response = []
+    for line in lines:
+        url_match = re.search(url_re, line)
+        this_line = {}
+        this_line['description'] = line
+        if url_match:
+            url = url_match.group()
+            this_line['url'] = url
+        response.append(this_line)
+
+    return json.dumps(response)
+
+@app.route('/urls/tags')
+def tags():
+    with open('urls.txt', 'r') as fh:
         data = fh.read()
     raw_tags = re.findall(r'#[a-zA-Z0-9_]+\b', data)
     ctr = collections.Counter()
@@ -18,14 +35,33 @@ def urls():
         ctr[tag] += 1
     return json.dumps(ctr)
 
-@app.route('/urls/<tag>')
+@app.route('/urls/search/<search>')
+def search(search):
+    with open('urls.txt', 'r') as fh:
+        lines = fh.readlines()
+
+    matched_lines = []
+    for line in lines:
+        m = re.findall(search, line)
+        if m:
+            url_match = re.search(url_re, line)
+            if url_match:
+                url = url_match.group()
+                matched_lines.append({
+                    'url':url,
+                    'description':line,
+                })
+
+    return json.dumps(matched_lines)
+
+@app.route('/urls/tag/<tag>')
 def tag(tag):
     with open('urls.txt', 'r') as fh:
         lines = fh.readlines()
 
     matched_lines = []
     for line in lines:
-        m = re.findall('#'+tag, line)
+        m = re.findall('#'+tag+r'\b', line)
         if m:
             url_match = re.search(url_re, line)
             if url_match:
