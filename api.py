1c3e5713b2513e3e8886ab1bab3d539e9812fec7
ryanfmurphy <ryanfmurphy@gmail.com>
2015-05-10 21:46:41 -0500

    made the /urls/search and /urls use the db instead of the urls.txt

--- api.py
+++ api.py
@@ -1,32 +1,25 @@
 from flask import Flask
 app = Flask(__name__)
 
-import json
-import re
-import collections
+import json, re, collections, sqlite3
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 tag_re = r'#[a-zA-Z0-9_]+\b'
 url_re = 'https?://\S+'
 
 @app.route('/urls')
 def urls():
-    with open('urls.txt', 'r') as fh:
-        lines = fh.readlines()
-
-    response = []
-    for line in lines:
-        url_match = re.search(url_re, line)
-        this_line = {}
-        this_line['description'] = line
-        if url_match:
-            url = url_match.group()
-            this_line['url'] = url
-        response.append(this_line)
-
-    return json.dumps(response)
+    db = sqlite3.connect('urls.db')
+    db.row_factory = dict_factory
+    return json.dumps(db.execute('SELECT * FROM url').fetchall())
 
 @app.route('/urls/tags')
-def tags():
+def tags(): #todo make tag associations and implement this
     with open('urls.txt', 'r') as fh:
         data = fh.read()
     raw_tags = re.findall(r'#[a-zA-Z0-9_]+\b', data)
@@ -37,22 +30,9 @@ def tags():
 
 @app.route('/urls/search/<search>')
 def search(search):
-    with open('urls.txt', 'r') as fh:
-        lines = fh.readlines()
-
-    matched_lines = []
-    for line in lines:
-        m = re.findall(search, line)
-        if m:
-            url_match = re.search(url_re, line)
-            if url_match:
-                url = url_match.group()
-                matched_lines.append({
-                    'url':url,
-                    'description':line,
-                })
-
-    return json.dumps(matched_lines)
+    db = sqlite3.connect('urls.db')
+    db.row_factory = dict_factory
+    return json.dumps(db.execute('SELECT * FROM url WHERE description LIKE ?', ('%'+search+'%',)).fetchall())
 
 @app.route('/urls/tag/<tag>')
 def tag(tag):
